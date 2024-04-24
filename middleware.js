1bce1c85ec642be42870bb20ab21894c365f2157
Alejandro Sánchez <alejandrosanchezb08@gmail.com>
2024-04-24 00:15:01 -0400

    Update allowedDomains in middleware.js to fix production domain

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ export default async function middleware(req) {
 
   // Define los dominios permitidos (localhost y dominio para producción)
   // Define allowed Domains (localhost and production domain)
-  const allowedDomains = ['localhost:3000', 'restaurantes-redo.vercel.app'];
+  const allowedDomains = ['localhost:3000', 'restaurantes.vercel.app'];
 
   // Verificamos si el hostname existe en los dominios permitidos
   // Verify if hostname exist in allowed domains
